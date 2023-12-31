1711ad146bb913421aab0ea6ebe45cb2ac065e94
DeltaV93 <valerie@techbychoice.org>
2023-12-31 12:15:55 -0800

    updated the UserProfileSerializer to only display DEI fields when the user set it to display

--- apps/core/serializers_member.py
+++ apps/core/serializers_member.py
@@ -13,6 +13,31 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+    def to_representation(self, instance):
+        ret = super(UserProfileSerializer, self).to_representation(instance)
+
+        if not instance.is_identity_sexuality_displayed:
+            ret.pop('identity_sexuality', None)
+
+        if not instance.is_identity_gender_displayed:
+            ret.pop('identity_gender', None)
+
+        if not instance.is_identity_ethic_displayed:
+            ret.pop('identity_ethic', None)
+
+        if not instance.is_pronouns_displayed:
+            ret.pop('identity_pronouns', None)
+
+        if not instance.is_disability_displayed:
+            ret.pop('disability', None)
+
+        if not instance.is_care_giver_displayed:
+            ret.pop('care_giver', None)
+
+        if not instance.is_veteran_status_displayed:
+            ret.pop('veteran_status', None)
+
+        return ret
 
 
 class TalentProfileSerializer(serializers.ModelSerializer):
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -17,13 +17,4 @@ urlpatterns = [
     path('profile/update/social-accounts', views.update_profile_social_accounts),
     path('profile/update/idenity', views.update_profile_identity),
     path('profile/update/notifications', views.update_profile_notifications),
-    # path('profile/update/mentor/career', views.update_profile_mentor_career),
-    # path('profile/update/mentor/value', views.update_profile_mentor_vallue),
-    # path('profile/update/mentor/mentor', views.update_profile_mentor),
-    # path('profile/update/mentor/mentee', views.update_profile_mentee),
-    # path('register/', views.register_user),
-    # path('logout/', knox_views.LogoutView.as_view()),
-    # path('logoutall/', knox_views.LogoutAllView.as_view()),
-    # path('pricing-plans/', views.PricingPlanList.as_view(), name='pricing-plan-list'),
-    # path('swh/', views.stripe_webhook)
 ]
