446d80cf0917568d9c0d544427307774fcf178e8
DeltaV93 <valerie@techbychoice.org>
2024-01-07 20:16:46 -0800

    moving to db env vars for deployment

--- api/settings.py
+++ api/settings.py
@@ -101,11 +101,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tbc_local',
-        'USER': 'valeriesharp',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
