3249d985fa00cec256ccb4d04f6c14b9d1a55073
Valerie Phoenix <valerie@techbychoice.org>
2024-01-25 19:02:28 -0800

    tbc-111
    added the talentprofile on creation, updated the mentor get call, made the code more dry (#44)

--- apps/core/views_member.py
+++ apps/core/views_member.py
@@ -1,4 +1,5 @@
 from django.core.serializers import serialize
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,74 +18,59 @@ class MemberDetailsView(APIView):
     """
     Retrieve CustomUser, UserProfile, and TalentProfile details.
     """
+    def get_profile(self, model, user):
+        try:
+            return model.objects.get(user=user)
+        except model.DoesNotExist:
+            return None
+
+    def get_current_company_data(self, user):
+        try:
+            company = CompanyProfile.objects.get(current_employees=user)
+            return {
+                "id": company.id,
+                "company_name": company.company_name,
+                "logo": company.logo.url,
+                "company_size": company.company_size,
+                "industries": [industry.name for industry in company.industries.all()]
+            }
+        except CompanyProfile.DoesNotExist:
+            return None
 
     def get(self, request, pk, format=None):
         try:
             user = CustomUser.objects.get(pk=pk)
-            user_profile = UserProfile.objects.get(user=user)
-            talent_profile = TalentProfile.objects.get(user=user)
-
-            try:
-                mentor_program = MentorshipProgramProfile.objects.get(user=user.pk)
-            except CompanyProfile.DoesNotExist:
-                mentor_program = None
-
-            try:
-                current_company = CompanyProfile.objects.get(current_employees=user)
-                current_company_data = {
-                    "id": current_company.id,
-                    "company_name": current_company.company_name,
-                    "logo": current_company.logo.url,
-                    "company_size": current_company.company_size,
-                    "industries": [industry.name for industry in current_company.industries.all()]
-                }
-            except CompanyProfile.DoesNotExist:
-                current_company = None
-                current_company_data = None
-
-            user_serializer = CustomUserSerializer(user)
-            user_profile_serializer = UserProfileSerializer(user_profile)
-            talent_profile_serializer = TalentProfileSerializer(talent_profile)
-
-            if mentor_program:
-                mentor_program_serializer = MentorshipProgramProfileSerializer(mentor_program)
-            else:
-                mentor_program_serializer = mentor_program
-
-            data = {
-                'user': user_serializer.data,
-                'user_profile': user_profile_serializer.data,
-                'talent_profile': talent_profile_serializer.data,
-                'current_company': current_company_data,
-                'mentorship_program': mentor_program_serializer.data
-            }
-
-            return Response({
-                'success': True,
-                'message': 'Member details retrieved successfully.',
-                'data': data
-            }, status=status.HTTP_200_OK)
-
         except CustomUser.DoesNotExist:
-            return Response({
-                'success': False,
-                'message': 'Member not found.'
-            }, status=status.HTTP_404_NOT_FOUND)
+            raise Http404('Member not found.')
 
-        except UserProfile.DoesNotExist:
-            return Response({
-                'success': False,
-                'message': 'User profile not found.'
-            }, status=status.HTTP_404_NOT_FOUND)
+        user_profile = self.get_profile(UserProfile, user)
+        if not user_profile:
+            raise Http404('User profile not found.')
 
-        except TalentProfile.DoesNotExist:
-            return Response({
-                'success': False,
-                'message': 'Talent profile not found.'
-            }, status=status.HTTP_404_NOT_FOUND)
+        talent_profile = self.get_profile(TalentProfile, user)
+        if not talent_profile:
+            raise Http404('Talent profile not found.')
 
-        except Exception as e:
-            return Response({
-                'success': False,
-                'message': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        mentor_program = self.get_profile(MentorshipProgramProfile, user)
+        current_company_data = self.get_current_company_data(user)
+
+        user_serializer = CustomUserSerializer(user)
+        user_profile_serializer = UserProfileSerializer(user_profile)
+        talent_profile_serializer = TalentProfileSerializer(talent_profile)
+        mentor_program_serializer = None
+        if mentor_program:
+            mentor_program_serializer = MentorshipProgramProfileSerializer(mentor_program)
+
+        data = {
+            'user': user_serializer.data,
+            'user_profile': user_profile_serializer.data,
+            'talent_profile': talent_profile_serializer.data,
+            'current_company': current_company_data,
+            'mentorship_program': mentor_program_serializer.data if mentor_program_serializer else None
+        }
+
+        return Response({
+            'success': True,
+            'message': 'Member details retrieved successfully.',
+            'data': data
+        }, status=status.HTTP_200_OK)
