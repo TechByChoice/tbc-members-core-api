9d051d5e021e3220227de0e7bb3aa8fe6f71f1de
DeltaV93 <valerie@techbychoice.org>
2024-02-08 12:27:02 -0800

    now sorting the dropdown items so they are in order for improved UX

--- apps/core/views_core.py
+++ apps/core/views_core.py
@@ -16,34 +16,34 @@ def get_dropdown_data(request):
         data['pronouns'] = list(PronounsIdentities.objects.values('name', 'id'))
 
     if not requested_fields or 'job_roles' in requested_fields:
-        data['job_roles'] = list(Roles.objects.values('name', 'id'))
+        data['job_roles'] = list(Roles.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'years_of_experience' in requested_fields:
         data['years_of_experience'] = [{'value': code, 'label': description} for code, description in CAREER_JOURNEY]
 
     if not requested_fields or 'companies' in requested_fields:
-        data['companies'] = list(CompanyProfile.objects.values('company_name', 'id', 'logo', 'company_url'))
+        data['companies'] = list(CompanyProfile.objects.order_by('company_name').values('company_name', 'id', 'logo', 'company_url'))
 
     if not requested_fields or 'job_skills' in requested_fields:
-        data['job_skills'] = list(Skill.objects.values('name', 'id'))
+        data['job_skills'] = list(Skill.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'job_departments' in requested_fields:
-        data['job_departments'] = list(Department.objects.values('name', 'id'))
+        data['job_departments'] = list(Department.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'job_industries' in requested_fields:
-        data['job_industries'] = list(Industries.objects.values('name', 'id'))
+        data['job_industries'] = list(Industries.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'company_types' in requested_fields:
-        data['company_types'] = list(CompanyTypes.objects.values('name', 'id'))
+        data['company_types'] = list(CompanyTypes.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'gender' in requested_fields:
-        data['gender'] = list(GenderIdentities.objects.values('name', 'id'))
+        data['gender'] = list(GenderIdentities.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'sexuality' in requested_fields:
-        data['sexuality'] = list(SexualIdentities.objects.values('name', 'id'))
+        data['sexuality'] = list(SexualIdentities.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'ethic' in requested_fields:
-        data['ethic'] = list(EthicIdentities.objects.values('name', 'id'))
+        data['ethic'] = list(EthicIdentities.objects.order_by('name').values('name', 'id'))
 
     if not requested_fields or 'job_salary_range' in requested_fields:
         data['job_salary_range'] = list(SalaryRange.objects.values('range', 'id'))
