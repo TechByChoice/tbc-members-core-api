975207ccd7d33093902dd1c44c6a12f19dd3fac9
Patrick Organ <patrick@permafrost.dev>
2024-03-30 16:55:06 -0400

    use env variable for whitelisted ip addresses

--- apps/core/firewall_middleware.py
+++ apps/core/firewall_middleware.py
@@ -1,3 +1,4 @@
+import os
 from django.http import HttpResponseForbidden
 
 class FirewallMiddleware:
@@ -5,11 +6,8 @@ class FirewallMiddleware:
     Middleware to restrict access to the application based on whitelisted IPs.
     """
 
-    # List of allowed IP addresses
-    ALLOWED_IPS = [
-        '127.0.0.1', '24.184.47.80', '75.223.174.33', '108.54.16.186',
-        '75.225.110.112', '97.133.52.183', '97.133.164.124'
-    ]
+    # List of allowed IP addresses, loaded from ALLOWED_IPS environment variable and split by comma:
+    ALLOWED_IPS = os.environ.get('ALLOWED_IPS', '').split(',')
 
     def __init__(self, get_response):
         self.get_response = get_response
