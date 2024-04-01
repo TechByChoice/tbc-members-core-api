96c558bf048cf925941c0ef6c959fef857a467f2
DeltaV93 <valerie@techbychoice.org>
2024-03-31 23:27:06 -0700

    removing firewall to fix prod

--- api/settings.py
+++ api/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     # firewall
-    "apps.core.firewall_middleware.FirewallMiddleware",
+    # "apps.core.firewall_middleware.FirewallMiddleware",
 ]
 
 ROOT_URLCONF = "api.urls"
