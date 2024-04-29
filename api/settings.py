e426484958a46db507b8ca898c6a2145518569fe
DeltaV93 <valerie@techbychoice.org>
2024-04-28 20:14:39 -0700

    fixing settings for local and prod/dev

--- api/settings.py
+++ api/settings.py
@@ -35,11 +35,16 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [
-    "beta.api.techbychoice.org",
-    "beta.api.dev.techbychoice.org"
-]
-
+if DEBUG:
+    ALLOWED_HOSTS = [
+        "localhost",
+        "127.0.0.1"
+    ]
+else:
+    ALLOWED_HOSTS = [
+        "beta.api.techbychoice.org",
+        "beta.api.dev.techbychoice.org"
+    ]
 # Application definition
 
 INSTALLED_APPS = [
@@ -202,25 +207,31 @@ CORS_ALLOW_METHODS = [
     "POST",
     "PUT",
 ]
-CORS_ALLOW_HEADERS = '*'
-# CORS_ALLOW_HEADERS = [
-#     'accept',
-#     'accept-encoding',
-#     'authorization',
-#     'content-type',
-#     'dnt',
-#     'origin',
-#     'user-agent',
-#     'x-csrftoken',
-#     'x-requested-with',
-# ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://www.beta.techbychoice.org",
-    "https://beta.techbychoice.org",
-    "https://www.opendoors.api.techbychoice.org",
-    "https://opendoors.api.techbychoice.org",
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
+
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+        "http://localhost:7000",
+    ]
+else:
+    CORS_ALLOWED_ORIGINS = [
+        "https://www.beta.techbychoice.org",
+        "https://beta.techbychoice.org",
+        "https://www.opendoors.api.techbychoice.org",
+        "https://opendoors.api.techbychoice.org",
+    ]
 
 # Allow cookies
 SESSION_COOKIE_SAMESITE = None
