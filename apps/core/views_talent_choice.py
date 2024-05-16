c08880613569392636b19b26c1e04d0a75259efa
Valerie Phoenix <valerie@techbychoice.org>
2024-05-15 18:05:52 -0700

    Update views_talent_choice.py

--- apps/core/views_talent_choice.py
+++ apps/core/views_talent_choice.py
@@ -144,6 +144,7 @@ class CompanyViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['get'], url_path='confirm-email/(?P<id>[^/]+)/(?P<token>[^/]+)')
     def confirm_account_email(self, request, id=None, token=None):
+        print("starting email confirmation flow")
         try:
             print(f"VALIDATE TOKEN: {token}")
             uid = force_str(urlsafe_base64_decode(id))
