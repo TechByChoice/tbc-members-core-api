7a33a98af83ba0225485584b09d21268a41bd453
DeltaV93 <valerie@techbychoice.org>
2024-05-18 21:00:53 -0700

    updated settings for cors issues

--- api/settings.py
+++ api/settings.py
@@ -35,26 +35,26 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if DEBUG:
-    ALLOWED_HOSTS = [
-        "localhost",
-        "127.0.0.1"
-    ]
-    CSRF_TRUSTED_ORIGINS = [
-        "http://localhost:3000",
-        "http://localhost:7000"
-    ]
-else:
-    ALLOWED_HOSTS = [
-        "beta-api.techbychoice.org",
-        "beta-api-dev.techbychoice.org"
-    ]
-    CSRF_TRUSTED_ORIGINS = [
-        "https://www.beta.techbychoice.org",
-        "https://beta.techbychoice.org",
-        "https://www.opendoors.api.techbychoice.org",
-        "https://opendoors.api.techbychoice.org",
-    ]
+# if DEBUG:
+#     ALLOWED_HOSTS = [
+#         "localhost",
+#         "127.0.0.1"
+#     ]
+#     CSRF_TRUSTED_ORIGINS = [
+#         "http://localhost:3000",
+#         "http://localhost:7000"
+#     ]
+# else:
+ALLOWED_HOSTS = [
+    "beta-api.techbychoice.org",
+    "beta-api-dev.techbychoice.org"
+]
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://www.beta.techbychoice.org",
+#     "https://beta.techbychoice.org",
+#     "https://www.opendoors.api.techbychoice.org",
+#     "https://opendoors.api.techbychoice.org",
+# ]
 # Application definition
 
 INSTALLED_APPS = [
