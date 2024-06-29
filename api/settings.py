c84ab3c07ee01bf285b4d0b4b5a345edcab563b5
Valerie <=>
2024-06-28 17:36:49 -0700

    fixing debug setting

--- api/settings.py
+++ api/settings.py
@@ -34,8 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == 'True'
 
 if DEBUG:
     ALLOWED_HOSTS = [
