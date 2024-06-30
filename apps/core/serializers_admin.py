a7a6bafa3e00d9c65b46f0aa2755bd27ccf69dff
Valerie <valerie@techbychoice.org>
2024-06-30 11:18:52 -0700

    removed old serializers

--- apps/core/serializers_admin.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class GenericBreakdownSerializer(serializers.Serializer):
-    name = serializers.CharField(read_only=True)  # Assuming each model has a name field
-    user_count = serializers.IntegerField()
